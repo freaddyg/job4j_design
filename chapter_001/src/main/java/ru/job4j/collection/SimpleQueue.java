446a1b5b0aacfca06c78bda96df68f6335885772
freaddyg <keepzor@mail.ru>
2020-10-16 12:03:52 +0300

    5. Очередь на двух стеках [#382034]

--- chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -3,23 +3,17 @@ package ru.job4j.collection;
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    private int sizeIn = 0;
-    private int sizeOut = 0;
 
     public T poll() {
-        if (sizeOut == 0) {
-            while (sizeIn != 0) {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
                 out.pushFirst(in.popFirst());
-                sizeIn--;
-                sizeOut++;
             }
         }
-        sizeOut--;
         return out.popFirst();
     }
 
     public void push(T value) {
         in.pushFirst(value);
-        sizeIn++;
     }
 }
--- chapter_001/src/main/java/ru/job4j/collection/SimpleStack.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleStack.java
@@ -6,19 +6,15 @@ import java.util.List;
 public class SimpleStack<T> {
     private ForwardLinked<T> linked = new ForwardLinked<T>();
 
-    public T pop() {
-        return linked.deleteLast();
-    }
-
     public T popFirst() {
         return linked.deleteFirst();
     }
 
-    public void pushFirst(T value) {
-        linked.addFirst(value);
+    public boolean isEmpty() {
+        return !linked.iterator().hasNext();
     }
 
-    public void push(T value) {
-        linked.add(value);
+    public void pushFirst(T value) {
+        linked.addFirst(value);
     }
 }
