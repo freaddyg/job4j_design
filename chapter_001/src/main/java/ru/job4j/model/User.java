764db70623393ce61c26273b1aa14778864138fc
freaddyg <keepzor@mail.ru>
2020-10-21 20:16:05 +0300

    2. Без переопределения equals и hashCode [#382064]

--- chapter_001/src/main/java/ru/job4j/model/User.java
+++ chapter_001/src/main/java/ru/job4j/model/User.java
@@ -1,5 +1,9 @@
 package ru.job4j.model;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class User {
     private String name;
     private int children;
@@ -33,5 +37,17 @@ public class User {
 
     public void setBirthday(Calendar birthday) {
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(new User("Yuriy", 1, new Calendar()), new Object());
+        map.put(new User("Evgeniy", 1, new Calendar()), new Object());
+
+        Set<User> users = map.keySet();
+        for (User u : users) {
+            System.out.println(u.getName() + " " + u.getChildren() + " " + u.getBirthday() + " " + map.get((User) u));
+        }
     }
 }
