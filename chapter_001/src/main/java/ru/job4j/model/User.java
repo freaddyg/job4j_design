1902a210cee6b0055dcb3c7f2e75770aca2205cb
freaddyg <keepzor@mail.ru>
2020-10-21 19:36:05 +0300

    1. Создать модель User [#382060]

--- chapter_001/src/main/java/ru/job4j/model/User.java
+++ chapter_001/src/main/java/ru/job4j/model/User.java
@@ -4,4 +4,34 @@ public class User {
     private String name;
     private int children;
     private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
 }
