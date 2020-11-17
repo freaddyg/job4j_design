b713e73affc2f0caed9c7856919b59854bf907d0
freaddyg <keepzor@mail.ru>
2020-11-17 17:02:09 +0300

    1. Бот [#382103]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -1,31 +1,45 @@
 package ru.job4j.io;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
 
         try (ServerSocket server = new ServerSocket(9000)) {
             while (true) {
-                StringBuilder stringRes = new StringBuilder();
+                String answer = "";
+                String[] word = new String[2];
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
                     while (!(str = in.readLine()).isEmpty()) {
-                        stringRes.append(str);
+                        if (str.contains("/?")) {
+                            String[] line = str.split("=");
+                            word = line[1].split(" ");
+                        }
                         System.out.println(str);
+
                     }
-                    if (stringRes.toString().contains("Bye")) {
+                    answer = word[0];
+                    if (word[0].equals("Hello")) {
+                        answer = "Hello";
+                    }
+                    if (word[0].equals("Bye")) {
                         break;
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(answer.getBytes());
                 }
             }
         }
