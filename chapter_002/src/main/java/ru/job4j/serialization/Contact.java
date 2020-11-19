158084c592a648379807bb27634d6e6adcaa731d
freaddyg <keepzor@mail.ru>
2020-11-19 12:07:13 +0300

    1. Что такое Сериализация? [#382092]

---
+++ chapter_002/src/main/java/ru/job4j/serialization/Contact.java
@@ -0,0 +1,51 @@
+package ru.job4j.serialization;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileAttribute;
+
+public class Contact implements Serializable {
+    private static final long serialVersionUID = 1234L;
+    private final int zipCode;
+    private final String phone;
+
+    public Contact(int zipCode, String phone) {
+        this.zipCode = zipCode;
+        this.phone = phone;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{"
+                + "zipCode=" + zipCode
+                + ", phone='" + phone
+                + '\'' + '}';
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        final Contact contact = new Contact(123456, "+7 (111) 111-11-11");
+        System.out.println(contact);
+        File tmp = Files.createTempFile("obj", ".tmp").toFile();
+        try (FileOutputStream fos = new FileOutputStream(tmp);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(contact);
+        }
+        try (FileInputStream fis = new FileInputStream(tmp);
+             ObjectInputStream ois =
+                     new ObjectInputStream(fis)) {
+            final Contact contactFromFile = (Contact) ois.readObject();
+            System.out.println(contactFromFile);
+        }
+    }
+}
